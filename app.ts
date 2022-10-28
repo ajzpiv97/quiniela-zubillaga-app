21aad4f500a3dec0e756cd2cd197cfca998ff2de
Armando Zubillaga <ajzpiv97@gmail.com>
2022-10-28 15:02:24 -0400

    Modified path

--- app.ts
+++ app.ts
@@ -4,7 +4,7 @@ const app = express();
 
 const path = require("path");
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3030;
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("build"));
