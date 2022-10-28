e629b1ecb36fdca6159f8d25cc97d3917b3fe4f6
Armando Zubillaga <ajzpiv97@gmail.com>
2022-10-28 15:42:35 -0400

    Modified again

--- app.ts
+++ app.ts
@@ -8,11 +8,8 @@ const port = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("build"));
-  app.get("/*", (req, res) => {
-    req.sendFile(path.resolve(__dirname, "build", "index.html"));
-  });
-  app.get("/login", (req, res) => {
-    req.sendFile(path.resolve(__dirname, "build", "index.html"));
+  app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
   });
 }
 
