2d12722be97bb40dda43103266f823f99ec7cc18
jcybul <46726471+jcybul@users.noreply.github.com>
2022-11-17 01:29:29 -0500

    add register api call

--- src/views/SignUp.tsx
+++ src/views/SignUp.tsx
@@ -40,6 +40,20 @@ export default function SignUp() {
       email: data.get("email"),
       password: data.get("password"),
     });
+
+    fetch('https://quiniela-zubillaga-api.herokuapp.com/auth/register', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ "email": data.get("email") ,
+                          "name": data.get("firstName"),
+                          "password" :data.get("password") ,
+                          "lastName":data.get("last_name")
+                            })
+})
+.then(response => response.json())
+.then(response => console.log(JSON.stringify(response)))
   };
 
   return (
