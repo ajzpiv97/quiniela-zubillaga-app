59f783e75e5524a206dce07b99869310a3e0ae88
Armando Zubillaga <ajzpiv97@gmail.com>
2022-11-22 12:15:16 -0500

    Removed typos

--- src/utils/authenticateUser.ts
+++ src/utils/authenticateUser.ts
@@ -8,19 +8,3 @@ export const parseJwt = (token: string | null) => {
     return null;
   }
 };
-
-// const VerifyUser = (props) => {
-//   React.useEffect(() => {
-//     const user = JSON.parse(localStorage.getItem("user"));
-
-//     if (user) {
-//       const decodedJwt = parseJwt(user.accessToken);
-
-//       if (decodedJwt.exp * 1000 < Date.now()) {
-//         props.logOut();
-//       }
-//     }
-//   }, [location, props]);
-
-//   return;
-// };
